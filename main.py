2aad507f45f367019021da7a059175a381829d42
Jesse Lynch <jesse.ro.lynch@gmail.com>
2019-04-02 21:52:36 -0500

    Updated save locations

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def train(epoch):
 
 def test(epoch):
     avg_psnr = 0
-    model = torch.load("siamese_checkpoints/model_epoch_{}.pth".format(epoch))
+    model = torch.load("siamese_rrcnn_checkpoints/model_epoch_{}.pth".format(epoch))
     model.to(device)
     int2Pred = []
     int1Pred = []
@@ -124,9 +124,9 @@ def test(epoch):
 
 
 def checkpoint(epoch):
-    if not os.path.exists('siamese_checkpoints'):
-        os.makedirs('siamese_checkpoints')
-    model_out_path = "siamese_checkpoints/model_epoch_{}.pth".format(epoch)
+    if not os.path.exists('siamese_rrcnn_checkpoints'):
+        os.makedirs('siamese_rrcnn_checkpoints')
+    model_out_path = "siamese_rrcnn_checkpoints/model_epoch_{}.pth".format(epoch)
     torch.save(model, model_out_path)
     print("Checkpoint saved to {}".format(model_out_path))
 
@@ -139,24 +139,24 @@ def main():
         epoch = opt.nEpochs
         int2Pred, int1Pred, lowPred, inputs, targets = test(epoch)
         x = (len(testing_data_loader.dataset))
-        if not os.path.exists('dataset/kidney/test/siamese_low_{}/'.format(epoch)):
-            os.makedirs('dataset/kidney/test/siamese_low_{}/'.format(epoch))
-        if not os.path.exists('dataset/kidney/test/siamese_int1_{}/'.format(epoch)):
-            os.makedirs('dataset/kidney/test/siamese_int1_{}/'.format(epoch))
-        if not os.path.exists('dataset/kidney/test/siamese_int2_{}/'.format(epoch)):
-            os.makedirs('dataset/kidney/test/siamese_int2_{}/'.format(epoch))
-        if not os.path.exists('dataset/kidney/test/siamese_input_{}/'.format(epoch)):
-            os.makedirs('dataset/kidney/test/siamese_input_{}/'.format(epoch))
-        if not os.path.exists('dataset/kidney/test/siamese_target_{}/'.format(epoch)):
-            os.makedirs('dataset/kidney/test/siamese_target_{}/'.format(epoch))
+        if not os.path.exists('dataset/kidney/test/siamese_rrcnn_low_{}/'.format(epoch)):
+            os.makedirs('dataset/kidney/test/siamese_rrcnn_low_{}/'.format(epoch))
+        if not os.path.exists('dataset/kidney/test/siamese_rrcnn_int1_{}/'.format(epoch)):
+            os.makedirs('dataset/kidney/test/siamese_rrcnn_int1_{}/'.format(epoch))
+        if not os.path.exists('dataset/kidney/test/siamese_rrcnn_int2_{}/'.format(epoch)):
+            os.makedirs('dataset/kidney/test/siamese_rrcnn_int2_{}/'.format(epoch))
+        if not os.path.exists('dataset/kidney/test/siamese_rrcnn_input_{}/'.format(epoch)):
+            os.makedirs('dataset/kidney/test/siamese_rrcnn_input_{}/'.format(epoch))
+        if not os.path.exists('dataset/kidney/test/siamese_rrcnn_target_{}/'.format(epoch)):
+            os.makedirs('dataset/kidney/test/siamese_rrcnn_target_{}/'.format(epoch))
         for i in range(x):
             lowres_fname = (test_set.lowres_filenames[i])
             fname = lowres_fname[27:39]
-            filename = 'dataset/kidney/test/siamese_low_{}/'.format(epoch) + str(fname)
-            i1filename = 'dataset/kidney/test/siamese_int1_{}/'.format(epoch) + str(fname)
-            i2filename = 'dataset/kidney/test/siamese_int2_{}/'.format(epoch) + str(fname)
-            in_filename = 'dataset/kidney/test/siamese_input_{}/'.format(epoch) + str(fname)
-            tg_filename = 'dataset/kidney/test/siamese_target_{}/'.format(epoch) + str(fname)
+            filename = 'dataset/kidney/test/siamese_rrcnn_low_{}/'.format(epoch) + str(fname)
+            i1filename = 'dataset/kidney/test/siamese_rrcnn_int1_{}/'.format(epoch) + str(fname)
+            i2filename = 'dataset/kidney/test/siamese_rrcnn_int2_{}/'.format(epoch) + str(fname)
+            in_filename = 'dataset/kidney/test/siamese_rrcnn_input_{}/'.format(epoch) + str(fname)
+            tg_filename = 'dataset/kidney/test/siamese_rrcnn_target_{}/'.format(epoch) + str(fname)
             print(filename)
             tv.save_image(inputs[i], in_filename)
             tv.save_image(targets[i], tg_filename)
